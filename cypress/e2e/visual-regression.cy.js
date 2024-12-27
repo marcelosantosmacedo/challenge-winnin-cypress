1753a0fd5a66f81f4cc8c8a15dbccd5a58046ecc
marcelosantosmacedo <159556068+marcelosantosmacedo@users.noreply.github.com>
2024-12-27 10:50:34 -0300

    Atualização da estrutura do projeto

--- cypress/e2e/visual-regression.cy.js
+++ cypress/e2e/visual-regression.cy.js
@@ -1,13 +1,13 @@
 /// <reference types="Cypress" />
 
-describe('Test Suite - Visual Testing', { tags: '@regression' }, function() {
+describe('Test Suite - Visual Testing', { tags: '@visual' }, function() {
 
     beforeEach(() => {
         cy.visit('https://www.wikipedia.org/')
       })
 
     describe('Regression Visual Testing', function() {
-      it('Verify if the page still the same', { tags: '@visual' }, function() {
+      it('Verify if the page still the same', function() {
         cy.compareSnapshot('home', {
           capture: 'fullPage',
           errorThreshold: 0.1
